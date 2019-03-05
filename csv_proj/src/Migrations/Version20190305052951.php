54176404faa25d630a3a4d753e67017baee2dba2
GaintVlad <sadkoff@tut.by>
2019-03-06 00:46:51 +0300

    add sql to create product table if it not exists

--- csv_proj/src/Migrations/Version20190305052951.php
+++ csv_proj/src/Migrations/Version20190305052951.php
@@ -22,7 +22,20 @@ final class Version20190305052951 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE tblProductData ADD strProductPrice INT UNSIGNED DEFAULT NULL, ADD strProductStock INT UNSIGNED DEFAULT NULL');
+        $this->addSql("CREATE TABLE IF NOT EXISTS tblProductData (
+              intProductDataId int(10) unsigned NOT NULL AUTO_INCREMENT,
+              strProductName varchar(50) NOT NULL,
+              strProductDesc varchar(255) NOT NULL,
+              strProductCode varchar(10) NOT NULL,
+              dtmAdded datetime DEFAULT NULL,
+              dtmDiscontinued datetime DEFAULT NULL,
+              stmTimestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+              PRIMARY KEY (intProductDataId),
+              UNIQUE KEY (strProductCode)
+            ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores product data';"
+
+            ."ALTER TABLE tblProductData ADD strProductPrice INT UNSIGNED DEFAULT NULL, ADD strProductStock INT UNSIGNED DEFAULT NULL;"
+        );
     }
 
     public function down(Schema $schema) : void
